6f888eedc6053789a0120ac88ae11446753ebe03
zzq <1032505101@qq.com>
2023-08-15 22:28:01 +0800

    init

---
+++ src/main/java/geekbang/designpatterns/GeekbangDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package geekbang.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekbangDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekbangDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geekbang/designpatterns/GeekbangDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package geekbang.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekbangDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
